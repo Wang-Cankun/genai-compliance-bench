67feda2bc343e06afb9459b0467af5d76e5f92e5
Zhengyi Zhang <yxszyxzz.love@qq.com>
2023-11-10 12:00:00 +0000

    Export core classes from package init

--- src/genai_compliance_bench/__init__.py
+++ src/genai_compliance_bench/__init__.py
@@ -1,3 +1,10 @@
 """genai-compliance-bench."""
 
 __version__ = "0.1.0"
+
+from genai_compliance_bench.policy_engine.engine import (
+    ComplianceResult, PolicyEngine, RiskLevel, Violation,
+)
+from genai_compliance_bench.policy_engine.rule_loader import RuleLoader
+
+__all__ = ["ComplianceResult", "PolicyEngine", "RiskLevel", "RuleLoader", "Violation"]
